b78d2a8709acacdfb922f8c631d6d11e314be1c4
Jay Patrick Cano <0x3EF8@gmail.com>
2022-04-13 10:16:18 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,4 +1,4 @@
-const live2d_path = "https://cdn.jsdelivr.net/gh/0x3EF8/Live2D-v20@master/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/0x3EF8/Live2D-v21@master/";
 //const live2d_path = "/live2d-widget/";
 
 // Encapsulate the method of asynchronously loading resources
