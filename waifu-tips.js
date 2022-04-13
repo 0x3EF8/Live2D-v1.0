66c1e6aee861fbbb5c87700d1792bc35b547c51a
Jay Patrick Cano <0x3EF8@gmail.com>
2022-04-13 09:25:24 +0800

    Add files via upload

--- waifu-tips.js
+++ waifu-tips.js
@@ -73,7 +73,7 @@ function loadWidget(config) {
             Live2D.captureFrame = true;
         });
         document.querySelector("#waifu-tool .fa-info-circle").addEventListener("click", () => {
-            open("https://github.com/stevenjoezhang/live2d-widget");
+            open("https://github.com/0x3EF8/Live2D-v3");
         });
         document.querySelector("#waifu-tool .fa-times").addEventListener("click", () => {
             localStorage.setItem("waifu-display", Date.now());
